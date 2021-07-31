bd0c0b5b629f676087c9026de65f7bcfdbf04868
Katharine Hammer <katharine.hammer@gmail.com>
2021-07-31 08:07:49 -0600

    repurposing project for Mile House

--- Assets/Scripts/Scriptable Objects/NodeConfig.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-
-[CreateAssetMenu(fileName = "NodeConfig", menuName = "Node Config", order = 0)]
-public class NodeConfig : ScriptableObject {
-    
-}
